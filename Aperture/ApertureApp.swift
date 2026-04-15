35f996dc7e7948b31979212a5e04331c583ac24b
Alexander Craig <alexrobcraig@googlemail.com>
2026-04-15 13:37:29 +0200

    Initial Commit

---
+++ Aperture/ApertureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApertureApp.swift
+//  Aperture
+//
+//  Created by Alexander Craig on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ApertureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
